7645939e66e0ff18db4c9095371a319a5092d894
CACTuS-AI <ali.ahmed@itu.edu.pk>
2023-08-22 13:12:21 +0500

    c

--- src/models/extraction.py
+++ src/models/extraction.py
@@ -20,6 +20,13 @@ def extract_text_from_pdf(pdf_path):
     return extracted_text
 
 
+
+
+
+
+
+
+
 if __name__ == '__main__':
     pdf_path = 'src/data/data_file.pdf'
     extracted_text = extract_text_from_pdf(pdf_path)
